4598e7b3052a8adae3fe9f0cf5f40a84277007e0
Karina Nakahara <81200562+nak4hara@users.noreply.github.com>
2023-05-04 20:18:41 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,17 @@ console.log("hi!")
 /*
 comentário com mais linhas
 */
+
+var clima = "Quente!"
+clima = "Frio"
+console.log(typeof clima)
+
+let age, isHuman, name
+age = 18;
+isHuman = true;
+name = 'João'
+console.log(clima, age, isHuman)
+console.log('O ' + name + 'tem ' + age + ' anos.') 
+
+//interpolando valores com template literals or template strings
+console.log(`o ${name} tem ${age} anos`)
